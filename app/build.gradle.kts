6a1629772623fbef6206b7bb2b442830d1cc6200
Черных Александр <razor01478920@mail.ru>
2024-02-13 20:35:16 +0300

    Поднял версию material3 и navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,19 +56,27 @@ android {
 }
 
 dependencies {
-
+    //Kotlin
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    //Jetpack Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-rc01")
-    implementation("com.github.RavenZIP:WorkShop:1.2.0")
 
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    //Material 3 элементы
+    implementation("androidx.compose.material3:material3:1.2.0")
 
+    //Библиотека кастомных элементов
+    implementation("com.github.RavenZIP:WorkShop:1.2.1")
+
+    //Навигация
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //Прочее
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
