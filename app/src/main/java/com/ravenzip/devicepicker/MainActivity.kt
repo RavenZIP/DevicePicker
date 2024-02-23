2999d50ee5b427e8a189cb83b71772b75faf9b1c
Черных Александр <razor01478920@mail.ru>
2024-02-23 13:53:02 +0300

    Добавил SplashScreenService

--- app/src/main/java/com/ravenzip/devicepicker/MainActivity.kt
+++ app/src/main/java/com/ravenzip/devicepicker/MainActivity.kt
@@ -3,16 +3,21 @@ package com.ravenzip.devicepicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.ravenzip.devicepicker.navigation.root.RootNavigationGraph
 import com.ravenzip.devicepicker.services.InitializeSnackBarIcons
+import com.ravenzip.devicepicker.services.SplashScreenService
 import com.ravenzip.devicepicker.ui.theme.DevicePickerTheme
 
 class MainActivity : ComponentActivity() {
+    private val splashScreenService: SplashScreenService by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +26,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val splashScreen = installSplashScreen()
+                    splashScreen.setKeepOnScreenCondition { splashScreenService.isLoading.value }
                     InitializeSnackBarIcons()
+
                     RootNavigationGraph(navController = rememberNavController())
                 }
             }
---
+++ app/src/main/java/com/ravenzip/devicepicker/services/SplashScreenService.kt
@@ -0,0 +1,20 @@
+package com.ravenzip.devicepicker.services
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SplashScreenService : ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _isLoading.value = false
+        }
+    }
+}
