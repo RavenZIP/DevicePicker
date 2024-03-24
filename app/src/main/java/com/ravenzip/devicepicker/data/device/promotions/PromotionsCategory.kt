e97a4f11c2c818f7ea6a064bd80430d52939649e
Черных Александр <razor01478920@mail.ru>
2024-03-24 11:07:53 +0300

    Удалил Promotions

--- app/src/main/java/com/ravenzip/devicepicker/data/device/promotions/PromotionsCategory.kt
+++
@@ -1,6 +0,0 @@
-package com.ravenzip.devicepicker.data.device.promotions
-
-// TODO: завести enum для type (0 - маленькие карточки, 1 - большие)
-class PromotionsCategory(val name: String, val type: Int, val position: Int) {
-    constructor() : this(name = "", type = 0, position = 0)
-}
