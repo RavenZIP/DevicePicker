86e15d10ec74cf5750a70a70117ed17a2fa6b27f
Черных Александр <razor01478920@mail.ru>
2024-07-14 10:46:18 +0300

    Добавил вывод информации об операционной системе

--- app/src/main/java/com/ravenzip/devicepicker/model/device/compact/DeviceSpecifications.kt
+++ app/src/main/java/com/ravenzip/devicepicker/model/device/compact/DeviceSpecifications.kt
@@ -4,22 +4,31 @@ import com.ravenzip.devicepicker.model.device.specifications.BaseInfo
 import com.ravenzip.devicepicker.model.device.specifications.BaseInfo.Companion.toMap
 import com.ravenzip.devicepicker.model.device.specifications.Construction
 import com.ravenzip.devicepicker.model.device.specifications.Construction.Companion.toMap
+import com.ravenzip.devicepicker.model.device.specifications.OperationSystem
+import com.ravenzip.devicepicker.model.device.specifications.OperationSystem.Companion.toMap
 import com.ravenzip.devicepicker.model.device.specifications.Screen
 import com.ravenzip.devicepicker.model.device.specifications.Screen.Companion.toMap
 
 class DeviceSpecifications(
     val baseInfo: BaseInfo,
     val screen: Screen,
-    val construction: Construction
+    val construction: Construction,
+    val operationSystem: OperationSystem
 ) {
-    constructor() : this(baseInfo = BaseInfo(), screen = Screen(), construction = Construction())
+    constructor() :
+        this(
+            baseInfo = BaseInfo(),
+            screen = Screen(),
+            construction = Construction(),
+            operationSystem = OperationSystem())
 
     companion object {
         fun DeviceSpecifications.toMap(): Map<String, Map<String, String>> {
             return mapOf(
                 "Общая информация" to this.baseInfo.toMap(),
                 "Экран" to this.screen.toMap(),
-                "Корпус и защита" to this.construction.toMap())
+                "Корпус и защита" to this.construction.toMap(),
+                "Операционная система" to this.operationSystem.toMap())
 
             // TODO добавить остальные характеристики
             // "Процессор", "Память", "Камера", "Аудио", "Сеть", "Питание", "Габариты, вес",
---
+++ app/src/main/java/com/ravenzip/devicepicker/model/device/specifications/OperationSystem.kt
@@ -0,0 +1,40 @@
+package com.ravenzip.devicepicker.model.device.specifications
+
+data class OperationSystem(
+    val typeOfOperationSystem: String,
+    val operationSystemVersion: Int,
+    val shellOfTheOperationSystem: String,
+    val shellVersion: Int,
+    val supportGoogleServices: Boolean
+) {
+    constructor() :
+        this(
+            typeOfOperationSystem = "Не определено",
+            operationSystemVersion = 0,
+            shellOfTheOperationSystem = "Не определено",
+            shellVersion = 0,
+            supportGoogleServices = true)
+
+    companion object {
+        fun OperationSystem.toMap(): Map<String, String> {
+            return mapOf(
+                "Операционная система" to this.typeOfOperationSystem,
+                "Версия ОС" to this.operationSystemVersion(),
+                "Оболочка" to this.shellOfTheOperationSystem,
+                "Версия оболочки" to this.shellVersion(),
+                "Поддержка Google Services" to supportGoogleServices())
+        }
+
+        fun OperationSystem.operationSystemVersion(): String {
+            return "${this.typeOfOperationSystem} ${this.operationSystemVersion}"
+        }
+
+        fun OperationSystem.shellVersion(): String {
+            return "${this.shellOfTheOperationSystem} ${this.shellVersion}"
+        }
+
+        fun OperationSystem.supportGoogleServices(): String {
+            return if (this.supportGoogleServices) "Есть" else "Отсутствует"
+        }
+    }
+}
