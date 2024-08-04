7c47d448d68e6a35c2a9d1b36903f36558961311
Черных Александр <razor01478920@mail.ru>
2024-08-04 08:50:06 +0300

    Подправил цвет ворнинг снэкбара и тега Энергоэффективный

--- app/src/main/java/com/ravenzip/devicepicker/ui/theme/Color.kt
+++ app/src/main/java/com/ravenzip/devicepicker/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.ravenzip.devicepicker.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val successColor = Color(0xFF49c472)
-val warningColor = Color(0xFFded147)
+val warningColor = Color(0xFFedbc1c)
 val errorColor = Color(0xFFbf242c)
 
 val gray = Color(0xFF3F4249)
@@ -17,7 +17,7 @@ val greenTag = Color(0xFF4ad96d)
 val popularTagColor = Color(0xFFed9455)
 val lowPriceTagColor = Color(0xFF4ad96d)
 val highPerformanceTagColor = Color(0xFFed5555)
-val energyEfficientTagColor = Color(0xFFc4b52d)
+val energyEfficientTagColor = Color(0xFFedbc1c)
 val newModelColor = Color(0xFFc455ed)
 val reliableColor = Color(0xFF5569ed)
 val highQualityScreenColor = Color(0xFFed5597)
