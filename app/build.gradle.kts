957556ec26d2c550b7a3953eeb300ea6dfae4a13
Черных Александр <razor01478920@mail.ru>
2024-10-30 23:32:07 +0300

    Поднял версию compileSdk и targetSdk до 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.ravenzip.devicepicker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ravenzip.devicepicker"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.7.0"
 
