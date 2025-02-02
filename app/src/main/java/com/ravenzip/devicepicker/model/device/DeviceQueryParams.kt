8b267520c56e626e86d9f16f3e174969eea890e2
Черных Александр <razor01478920@mail.ru>
2025-02-02 13:58:09 +0300

    Убрал DeviceQueryParams (не используется)

--- app/src/main/java/com/ravenzip/devicepicker/model/device/DeviceQueryParams.kt
+++
@@ -1,5 +0,0 @@
-package com.ravenzip.devicepicker.model.device
-
-import androidx.compose.runtime.Immutable
-
-@Immutable class DeviceQueryParams(val uid: String, val brand: String, val model: String)
