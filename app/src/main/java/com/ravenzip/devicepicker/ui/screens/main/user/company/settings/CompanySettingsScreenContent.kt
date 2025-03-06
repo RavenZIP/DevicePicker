b4f653cdff0af28f3fbbdf48f87c28313be08593
Черных Александр <razor01478920@mail.ru>
2025-03-06 21:00:04 +0300

    Наработки по экрану с настройками компании

--- app/src/main/java/com/ravenzip/devicepicker/ui/screens/main/user/company/settings/CompanySettingsScreenContent.kt
+++ app/src/main/java/com/ravenzip/devicepicker/ui/screens/main/user/company/settings/CompanySettingsScreenContent.kt
@@ -1,6 +1,24 @@
 package com.ravenzip.devicepicker.ui.screens.main.user.company.settings
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ravenzip.devicepicker.ui.screens.main.user.company.viewmodel.CompanyInfoViewModel
 
-@Composable fun CompanySettingsScreenContent(viewModel: CompanyInfoViewModel,) {}
+@Composable
+fun CompanySettingsScreenContent(viewModel: CompanyInfoViewModel) {
+    val company = viewModel.company.collectAsStateWithLifecycle().value
+
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        items(company.settings) { setting ->
+            // TODO нужны доработки из WorkShop с новым контролом множественного выбора
+        }
+    }
+}
