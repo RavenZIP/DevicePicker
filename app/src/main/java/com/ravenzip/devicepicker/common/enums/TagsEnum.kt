e1f5f8df39ad16c1b8e563f33a2ed8386a3e5ff6
Черных Александр <razor01478920@mail.ru>
2025-05-25 15:25:24 +0300

    Скорректирован порядок меток на главном экране

--- app/src/main/java/com/ravenzip/devicepicker/common/enums/TagsEnum.kt
+++ app/src/main/java/com/ravenzip/devicepicker/common/enums/TagsEnum.kt
@@ -2,6 +2,16 @@ package com.ravenzip.devicepicker.common.enums
 
 import androidx.compose.ui.graphics.Color
 import com.ravenzip.devicepicker.R
+import com.ravenzip.devicepicker.common.enums.TagsEnum.HEAVY
+import com.ravenzip.devicepicker.common.enums.TagsEnum.HIGH_PERFORMANCE
+import com.ravenzip.devicepicker.common.enums.TagsEnum.LIGHT_WEIGHT
+import com.ravenzip.devicepicker.common.enums.TagsEnum.LOW_QUALITY
+import com.ravenzip.devicepicker.common.enums.TagsEnum.NEW_MODEL
+import com.ravenzip.devicepicker.common.enums.TagsEnum.OLD_MODEL
+import com.ravenzip.devicepicker.common.enums.TagsEnum.POPULAR
+import com.ravenzip.devicepicker.common.enums.TagsEnum.RELIABLE
+import com.ravenzip.devicepicker.common.enums.TagsEnum.SECURITY
+import com.ravenzip.devicepicker.common.enums.TagsEnum.USER_SELECTION
 import com.ravenzip.devicepicker.common.theme.heavyTagColor
 import com.ravenzip.devicepicker.common.theme.highPerformanceTagColor
 import com.ravenzip.devicepicker.common.theme.lightWeightTagColor
@@ -45,34 +55,31 @@ enum class TagsEnum(val value: String, val description: String, val color: Color
         color = popularTagColor,
         icon = R.drawable.i_flame,
     ),
-    HIGH_PERFORMANCE(
-        value = "Производительный",
-        description =
-            "Данное устройство считается производительным, " +
-                "поскольку его характеристики лучше, " +
-                "чем у большинства других устройств",
-        color = highPerformanceTagColor,
-        icon = R.drawable.i_speedometer,
-    ),
-    RELIABLE(
-        value = "Надежный",
-        description =
-            "Данное устройство считается надежным, " +
-                "поскольку рейтинг устройства превышает 4,5 балла",
-        color = reliableColor,
-        icon = R.drawable.i_trust,
-    ),
     NEW_MODEL(
         value = "Новинка",
         description = "Данная модель считается новой, поскольку ее выпуск состоялся в текущем году",
         color = newModelColor,
         icon = R.drawable.i_new,
     ),
-    OLD_MODEL(
-        value = "Устаревший",
-        description = "Данная модель считается устаревшей, поскольку вышла 4 или более лет назад",
-        color = oldModelTagColor,
-        icon = R.drawable.i_history,
+    HIGH_PERFORMANCE(
+        value = "Производительный",
+        description =
+            "Данное устройство считается производительным, поскольку его характеристики лучше, " +
+                "чем у большинства других устройств",
+        color = highPerformanceTagColor,
+        icon = R.drawable.i_speedometer,
+    ),
+    USER_SELECTION(
+        value = "Выбор пользователей",
+        description = "Описание...",
+        color = userSelectionTagColor,
+        icon = R.drawable.i_medal,
+    ),
+    RELIABLE(
+        value = "Надежный",
+        description = "Описание...",
+        color = reliableColor,
+        icon = R.drawable.i_trust,
     ),
     LIGHT_WEIGHT(
         value = "Легкий",
@@ -80,28 +87,28 @@ enum class TagsEnum(val value: String, val description: String, val color: Color
         color = lightWeightTagColor,
         icon = R.drawable.i_light_weight,
     ),
-    HEAVY(
-        value = "Тяжелый",
-        description = "У данной модели тяжелый вес",
-        color = heavyTagColor,
-        icon = R.drawable.i_heavy,
-    ),
     SECURITY(
         value = "Защищенный",
         description = "Данная модель считается защищенной, поскольку...",
         color = securityTagColor,
         icon = R.drawable.i_security,
     ),
+    OLD_MODEL(
+        value = "Устаревший",
+        description = "Данная модель считается устаревшей, поскольку вышла 4 или более лет назад",
+        color = oldModelTagColor,
+        icon = R.drawable.i_history,
+    ),
+    HEAVY(
+        value = "Тяжелый",
+        description = "У данной модели тяжелый вес",
+        color = heavyTagColor,
+        icon = R.drawable.i_heavy,
+    ),
     LOW_QUALITY(
         value = "Низкое качество",
         description = "Описание...",
         color = lowQualityColor,
         icon = R.drawable.i_problem_report,
-    ),
-    USER_SELECTION(
-        value = "Выбор пользователей",
-        description = "Описание...",
-        color = userSelectionTagColor,
-        icon = R.drawable.i_medal,
     ),
 }
