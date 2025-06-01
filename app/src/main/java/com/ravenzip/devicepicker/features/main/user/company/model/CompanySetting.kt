d5b440138e95a612b97010220f084ee57cb22281
Черных Александр <razor01478920@mail.ru>
2025-06-01 08:51:38 +0300

    Инициализация полей класса (иначе падало)

---
+++ app/src/main/java/com/ravenzip/devicepicker/features/main/user/company/model/CompanySetting.kt
@@ -0,0 +1,8 @@
+package com.ravenzip.devicepicker.features.main.user.company.model
+
+data class CompanySetting(
+    val name: String = "",
+    val code: Int = 0,
+    val description: String = "",
+    val active: Boolean = false,
+)
--- app/src/main/java/com/ravenzip/devicepicker/features/main/user/company/model/CompanySettings.kt
+++
@@ -1,8 +0,0 @@
-package com.ravenzip.devicepicker.features.main.user.company.model
-
-data class CompanySettings(
-    val name: String,
-    val code: Int,
-    val description: String,
-    val active: Boolean,
-)
